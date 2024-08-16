cf969ba047bd9b298185f7b02d689434e5d85960
Peter MacIntyre <pbmacintyre@gmail.com>
2024-08-16 16:47:01 -0300

    more setting up the webhook to respond to specific events

--- account_info_tester.php
+++ account_info_tester.php
@@ -7,10 +7,8 @@ require_once('includes/ringcentral-db-functions.inc');
 show_errors();
 
 $sdk = ringcentral_sdk();
-//echo_spaces("SDK object", $sdk);
 
 $endpoint = "/restapi/v1.0/account/~/directory/entries";
-//$endpoint = "/restapi/v1.0/account/~/extension/~/assigned-role";
 
 $params = array(
     'showFederated' => 'true',
@@ -18,27 +16,49 @@ $params = array(
     'typeGroup' => '',
     'page' => 1,
     'perPage' => 'all',
-    'siteId' => ''
+    'siteId' => '',
 );
 
-//$params = null ;
-//echo_spaces("params", $params);
-
 try {
-//    $resp = $sdk->platform()->get($endpoint);
     $resp = $sdk->platform()->get($endpoint, $params);
-    // echo_spaces("Response Object", $resp->json()->records);
-} catch (Exception $e) {
-    echo_spaces("Error Message", $e->getMessage());
+//     echo_spaces("Response Object", $resp->json()->records);
+}
+catch (Exception $e) {
+    echo_spaces("Error Message 1", $e->getMessage());
 }
 
+$i = 1;
+//
 foreach ($resp->json()->records as $value) {
-    if ($value->extensionNumber == '12001') {
-        echo_spaces("ID", $value->id);
-        echo_spaces("first Name", $value->firstName);
-        echo_spaces("last Name", $value->lastName);
-        echo_spaces("email", $value->email);
-        echo_spaces("Account ID", $value->account->id,1);
+    if ($value->type == 'User' && $value->status == 'Enabled' && $value->phoneNumbers[0]->phoneNumber ) {
+        // if they are enabled user types with a phone number
+        $params2 = array(
+            'extensionNumber' => $value->extensionNumber,
+        );
+        $accountID = '3058829020';
+        sleep(1) ;
+        try {
+            $endpoint2 = "/restapi/v1.0/account/$accountID/extension";
+            $resp2 = $sdk->platform()->get($endpoint2, $params2);
+            foreach ($resp2->json()->records as $value2) {
+                if ($value2->permissions->admin->enabled) {
+                    // if they are admin level
+                    echo_spaces("Counter", $i++);
+                    echo_spaces("ID", $value->id);
+                    echo_spaces("first Name", $value->firstName);
+                    echo_spaces("last Name", $value->lastName);
+                    echo_spaces("Extension #", $value->extensionNumber);
+                    echo_spaces("email", $value->email);
+                    echo_spaces("phone #", $value->phoneNumbers[0]->phoneNumber);
+                    echo_spaces("Account ID", $value->account->id);
+                    echo_spaces("Admin Permission", $value2->permissions->admin->enabled, 1);
+                }
+            }
+//          echo_spaces("Response Object", $resp2->json()->records,1);
+        }
+        catch (Exception $e) {
+            echo_spaces("Error Message resp2", $e->getMessage());
+        }
     }
 }
 
